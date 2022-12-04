3441e2e9ca6272b9a4229ee40107e1f50383ba84
Fredrik Furufors <furufors@gmail.com>
2022-12-04 14:52:34 +0100

    Added day 1

--- day01/day1p1.hs
+++ day01/day1p1.hs
@@ -1,4 +1,10 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
 main :: IO ()
-main = undefined
+main = interact $ show . find . map read . lines
+
+find :: [Int] -> Int
+find [    ] = 0
+find (a:as) = case [a * i | i <- as, a+i == 2020] of
+                [   ] -> find as
+                (s:_) -> s
--- day01/day1p2.hs
+++ day01/day1p2.hs
@@ -1,4 +1,10 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
 main :: IO ()
-main = undefined
+main = interact $ show . find . map read . lines
+
+find :: [Int] -> Int
+find [    ] = 0
+find (a:as) = case [a * i * j | i <- as, j <- as, i /= j, a+i+j == 2020] of
+                [   ] -> find as
+                (s:_) -> s
