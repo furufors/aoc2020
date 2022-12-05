0bc8e9a6374749e36308e053831a0ec5c0408060
Fredrik Furufors <furufors@gmail.com>
2022-12-05 21:28:27 +0100

    Added day 3

--- day03/day3p1.hs
+++ day03/day3p1.hs
@@ -1,4 +1,10 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
 main :: IO ()
-main = undefined
+main = interact $ show . ride 0 0 . lines
+
+ride :: Int -> Int -> [String] -> Int
+ride x y ss = let x' = x `mod` (length $ head ss)
+              in if y >= length ss
+                 then 0
+                 else (if ss!!y!!x' == '.' then 0 else 1) + ride (x+3) (y+1) ss
--- day03/day3p2.hs
+++ day03/day3p2.hs
@@ -1,4 +1,13 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
 main :: IO ()
-main = undefined
+main = interact $ show . foldl1 (*) . rides . lines
+
+rides :: [String] -> [Int]
+rides ss = [ride 0 0 dx dy ss | (dx,dy) <- [(1,1),(3,1),(5,1),(7,1),(1,2)]]
+
+ride :: Int -> Int -> Int -> Int -> [String] -> Int
+ride x y dx dy ss = let x' = x `mod` (length $ head ss)
+                    in if y >= length ss
+                       then 0
+                       else (if ss!!y!!x' == '.' then 0 else 1) + ride (x+dx) (y+dy) dx dy ss
