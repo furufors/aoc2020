cd3bf24e5464515022065886fbe202ff1c6bcaf8
Fredrik Furufors <furufors@gmail.com>
2022-12-12 21:18:04 +0100

    Added day 10

--- day10/day10p1.hs
+++ day10/day10p1.hs
@@ -1,4 +1,12 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.List
+
 main :: IO ()
-main = undefined
+main = interact $ show . calculate . sort . map read . lines
+
+calculate :: [Int] -> Int
+calculate is = let is' = map (\(a,b) -> a - b) $ zip (is++[maximum is + 3]) (0:is)
+                   ones = length $ filter (==1) is'
+                   threes = length $ filter (==3) is'
+               in ones * threes
