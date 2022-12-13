8eb0bba5d969564abeed98a872788aacb80241ce
Fredrik Furufors <furufors@gmail.com>
2022-12-13 19:40:12 +0100

    Added day 10

--- day10/day10p2.hs
+++ day10/day10p2.hs
@@ -1,4 +1,16 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.List
+
 main :: IO ()
-main = undefined
+main = interact $ show . calculate . sort . addSocket . map read . lines
+
+calculate :: [Int] -> Int
+calculate is = memoized_cal 0
+    where
+        memoized_cal = (map calculate' [0 ..]!!)
+            where
+                calculate' i = if i == maximum is then 1 else sum [ memoized_cal e | e <- [f | f <- is, (f - i) `elem` [1,2,3]]]
+
+addSocket :: [Int] -> [Int]
+addSocket is = 0:(maximum is + 3):is
