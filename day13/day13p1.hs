cb076b1ac23e77446f93b901429db0ede2ff7a28
Fredrik Furufors <furufors@gmail.com>
2023-03-18 15:10:15 +0100

    Added day 13

--- day13/day13p1.hs
+++ day13/day13p1.hs
@@ -1,4 +1,32 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Text.Parsec
+import Data.List
+import Data.Ord
+
 main :: IO ()
-main = undefined
+main = interact $ show . (\(b,t) -> b*t) . head . sortBy (comparing snd) . toWait . parseLines . lines
+
+toWait :: (Int, [Int]) -> [(Int, Int)]
+toWait (t,bs) = map (\b -> (b, b - (t `mod` b))) bs
+
+parseLines :: [String] -> (Int, [Int])
+parseLines ls = (read . head $ ls, parseBusses . last $ ls)
+
+parseBusses :: String -> [Int]
+parseBusses input = case parse parseBusses2 "parsein" input of
+    Left err -> error $ show err
+    Right a -> a
+
+parseBusses2 :: Parsec String () [Int]
+parseBusses2 = do
+    busses  <- (try busNumber <|> try busX) `sepBy` (string ",")
+    return $ concat busses
+
+busNumber :: Parsec String () [Int]
+busNumber = (:[]) . read <$> many1 digit
+
+busX :: Parsec String () [Int]
+busX = do
+    _ <- string "x"
+    return []
--- day13/day13p2.hs
+++ day13/day13p2.hs
@@ -1,4 +1,45 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Text.Parsec
+import Data.List
+import Data.Maybe
+import Data.Ord
+
 main :: IO ()
-main = undefined
+main = interact $ show . solve . parseLine . last . lines
+
+solve :: [(Int, Int)] -> Int
+solve ps = solve' 1 (head ps) (tail ps)
+    where
+        solve' n (b, o) ps =
+            let time = n * 13 * b - o
+            in if all (departAt time) ps
+               then time
+               else solve' (n+1) (b, o) ps
+        departAt i (b, o) = (i+o) `mod` b == 0
+
+parseLine :: String -> [(Int, Int)]
+parseLine ls = reverse . sortBy (comparing fst) . filterJusts $ zip (parseBusses ls) [0..]
+
+filterJusts :: [(Maybe Int, Int)] -> [(Int, Int)]
+filterJusts ((Just i,j)  :ps) = (i,j):(filterJusts ps)
+filterJusts ((Nothing, j):ps) = filterJusts ps
+filterJusts [               ] = []
+
+parseBusses :: String -> [Maybe Int]
+parseBusses input = case parse parseBusses2 "parsein" input of
+    Left err -> error $ show err
+    Right a -> a
+
+parseBusses2 :: Parsec String () [Maybe Int]
+parseBusses2 = do
+    busses  <- (try busNumber <|> try busX) `sepBy` (string ",")
+    return $ busses
+
+busNumber :: Parsec String () (Maybe Int)
+busNumber = Just . read <$> many1 digit
+
+busX :: Parsec String () (Maybe Int)
+busX = do
+    _ <- string "x"
+    return Nothing
